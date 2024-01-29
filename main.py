d4c13529918d7b3e2763f3299dfb8b9718100bf6
Arturo Suárez <manuel.suarez@cimat.mx>
2024-01-28 23:36:34 -0600

    Loading datasets

--- main.py
+++ main.py
@@ -1,9 +1,65 @@
 import tensorflow as tf
+import os
+import random
+import numpy as np
+
+from tqdm import tqdm
+
+from skimage.io import imread, imshow
+from skimage.transform import resize
+import matplotlib.pyplot as plt.pyplot as plt
+
+seed = 42
+np.random.seed = seed
 
 # Parameters definition
 IMG_WIDTH = 128
 IMG_HEIGHT = 128
 IMG_CHANNELS = 3
+
+TRAIN_PATH = 'stage1_train/'
+TEST_PATH = 'stage1_test/'
+
+train_ids = next(os.walk(TRAIN_PATH))[1]
+test_ids = next(os.walk(TEST_PATH))[1]
+
+X_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+Y_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, 1), type=np.bool)
+
+print('Resizing training images and masks')
+for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
+    path = TRAIN_PATH + id_
+    img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
+    img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+    X_train[n] = img #Fill empty X_train with values from img
+    mask = np.zeros((IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+    for mask_file in next(os.walk(path + '/masks/'))[2]:
+        mask_ = imread(path + '/masks/' + mask_file)
+        mask_ = np.expand_dims(resize(mask_, (IMG_HEIGHT, IMG_WIDTH), mode='constant',
+                                      preserve_range=True), axis=-1)
+        mask = np.maximum(mask, mask_)
+    Y_train[n] = mask
+
+# test images
+X_test = np.zeros((len(test_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+sizes_test = []
+print('Resizing test images')
+for n, id_ in tqdm(enumerate(test_ids), total=len(test_ids)):
+    path = TEST_PATH + id_
+    img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
+    sizes_test.append([img.shape[0], img.shape[1]])
+    img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+    X_test[n] = img
+
+print('Done!')
+
+image_x = random.randint(0, len(train_ids))
+plt.imshow(X_train[image_x])
+plt.savefig('figure01.png')
+plt.close()
+plt.imshow(np.squeeze(Y_train[image_x]))
+plt.savefig('figure02.png')
+plt.close()
 
 # Model building
 inputs = tf.keras.layers.Input(shape=(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
